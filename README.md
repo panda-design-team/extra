a122ce029a0b7d00c2f96a829079430d012c0492
Cong Zhang <dancerphil1994@gmail.com>
2023-07-04 11:48:11 +0800

    0.0.7

--- README.md
+++ README.md
@@ -8,6 +8,20 @@ Extra components and utils of [Panda Design](https://github.com/panda-design-tea
 
 English | [中文](https://github.com/panda-design-team/extra/blob/main/docs/README-zh_CN.md)
 
+# @panda-design/router
+
+### 干嘛的？
+
+与 `react-router-dom` 的 `data API` 联动，可以很方便的生成 `Breadcrumbs 面包屑` 和 `documentTitle 浏览器标签名`
+
+### 为什么是一个单独的库？
+
+此库额外依赖了 `react-router-dom` 作为 `peerDependencies`，可能会影响到主组件库的升级。
+
+### 注意事项
+
+`antd@5.3.2` + `react-router-dom@6.8.2` 是验证可行的。往上的版本可以自行测试，一般来说都是可以的，往下的版本也可以自行测试，一般不行。
+
 ## Get Started
 
 - install
--- src/index.ts
+++ src/index.ts
@@ -4,13 +4,20 @@ import {
     LeftNavigationMenuItem,
     LeftNavigationDividerItem
 } from './LeftNavigation/interface';
+import {useRouteBreadcrumbItems} from './RouteConfig/useRouteBreadcrumbItems';
+import {useRouteDocumentTitle} from './RouteConfig/useRouteDocumentTitle';
+import {RouteObject, BreadcrumbMenuItem} from './RouteConfig/interface';
 
 export type {
     LeftNavigationProps,
     LeftNavigationMenuItem,
-    LeftNavigationDividerItem
+    LeftNavigationDividerItem,
+    RouteObject,
+    BreadcrumbMenuItem
 }
 
 export {
-    LeftNavigation
+    LeftNavigation,
+    useRouteBreadcrumbItems,
+    useRouteDocumentTitle
 }
